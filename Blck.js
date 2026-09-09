7e1c30b8a833f7c8c3161ef2c00683d84b6593be
tinophan3108066 <159065484+tinophan3108066@users.noreply.github.com>
2026-09-09 14:37:07 +0700

    Add Blck.js to handle blocked website response

---
+++ Blck.js
@@ -0,0 +1,7 @@
+$done({
+    response: {
+        status: 200,
+        headers: { "Content-Type": "text/html; charset=utf-8" },
+        body: "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'></head><body style='text-align:center;font-family:sans-serif;margin-top:50px;'><h2>Trang web này đã bị chặn bởi Script Shadowrocket.</h2></body></html>"
+    }
+});
